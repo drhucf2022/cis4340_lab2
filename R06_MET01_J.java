096f087dac4de4e6d00d2dd050295835f4363c5a
Donald Hires <donnierhires@knights.ucf.edu>
2026-03-08 23:37:58 -0400

    Apply secure coding standard to the one example I did not save before the previous commit :sob:

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -9,9 +9,21 @@
 public class R06_MET01_J {
 
     public static int getAbsAdd(int x, int y) {
-        return Math.abs(x) + Math.abs(y);
+
+        if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+            throw new IllegalArgumentException();
+        }
+
+        int absX = Math.abs(x);
+        int absY = Math.abs(y);
+
+        if (absX > Integer.MAX_VALUE - absY) {
+            throw new IllegalArgumentException();
+        }
+    
+        return absX + absY;
+    
     }
 
-    int answer = getAbsAdd(Integer.MIN_VALUE, 1);
-    
+
 }
